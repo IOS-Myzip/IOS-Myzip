2b4c4b3c07f14d417d5bc984545a57fc177e0b6f
a-neey <2371429@hansung.ac.kr>
2026-05-24 16:03:34 +0900

    Initial Commit

---
+++ LinkDeck/LinkDeckApp.swift
@@ -0,0 +1,17 @@
+//
+//  LinkDeckApp.swift
+//  LinkDeck
+//
+//  Created by 김예나 on 5/24/26.
+//
+
+import SwiftUI
+
+@main
+struct LinkDeckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
